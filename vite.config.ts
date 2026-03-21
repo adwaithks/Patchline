caf4a9d8530b494549d023b7a2a75dd1547cbfd3
adwaithks <adwaith2507@gmail.com>
2026-03-22 01:51:01 +0530

    install shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
 	plugins: [react()],
 	root: "src/mainview",
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src/mainview"),
+		},
+	},
 	build: {
 		outDir: "../../dist",
 		emptyOutDir: true,
